b63aa6442135907cacc0a765eb7241c07b837ce3
Paul Beard <paulbread88@gmail.com>
2026-02-14 15:20:39 +0000

    Reduce height of bars in tables. Slight article changes

--- style_table.py
+++ style_table.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
-
 import pandas as pd
 
-def style_table(sdf, bar_color="forestgreen", max_width=None):
+
+def style_table(sdf, bar_color="forestgreen", max_width=None, bar_subset=None):
 
     scores_subset = pd.IndexSlice[:, ("Scores", slice(None))]
+
+    if bar_subset is None:
+        bar_subset = scores_subset
 
     styler = (
         sdf.style
         .format("{:.0%}", subset=scores_subset)
         .format("{:.0f}", subset=pd.IndexSlice[:, ("Count", slice(None))])
-        .bar(scores_subset, vmin=0, vmax=1, color=bar_color)
+        .bar(bar_subset, vmin=0, vmax=1, color=bar_color, height=80)
 
         .set_table_styles([
             {
                 "selector": "thead th",
                 "props": [
-                    ("background", bar_color),
-                    ("color", "white"),
+                    ("background", "#d3d3d3"),
+                    ("color", "#333333"),
                     ("font-weight", "400"),
                     ("text-align", "center"),
                     ("padding", "8px"),
@@ -36,7 +39,7 @@ def style_table(sdf, bar_color="forestgreen", max_width=None):
                     ("left", "5%"),
                     ("right", "5%"),
                     ("bottom", "1px"),
-                    ("border-bottom", "1px solid #ededed"),
+                    ("border-bottom", "1px solid #b0b0b0"),
                 ],
             },
 
@@ -74,4 +77,3 @@ def style_table(sdf, bar_color="forestgreen", max_width=None):
         style_attr += f"max-width:{max_width};"
 
     return styler.set_table_attributes(f'style="{style_attr}"')
-
